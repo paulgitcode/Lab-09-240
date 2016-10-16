c9953ddfd1b979b650f0dc9cac8517551a03d0bb
paulgitcode <paul.temo@live.com>
2016-10-16 18:54:10 -0400

    Updated

--- src/myJPanelstd.java
+++ src/myJPanelstd.java
@@ -1,28 +1,36 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class myJPanelstd extends JPanel implements ActionListener
 {
+    student st1 = new student("Tom", "Jerry", 44);
+    
 	public myJPanelstd()
 	{
             super();
             setBackground(Color.pink);
+            setLayout(new BorderLayout());
             
-            student st1 = new student("Tom", "Jerry", 44);
             
 	    //JButton jb1;
    	    //jb1 = new JButton();
 //-------------------------------------------------------	    
 // add buttons to JPanel		
 //-------------------------------------------------------	    
-		add(st1);
+		add(st1, "North");
                 st1.setBackground(Color.gray);
                 st1.setText(st1.getN());
+                st1.addActionListener(this);
 	}
-        
+       @Override 
         public void actionPerformed(ActionEvent e)
         {
-            
+           st1.setText(st1.getInfo());
+           st1.setBackground(Color.lightGray);
         }
+
+    
+
 }
